0b159352a27c2894ce732063bbdd1d3869acec4f
ohchanKyu <okc0202@naver.com>
2024-06-07 09:30:18 +0900

    Edit Rest API URL

--- soll-parking/src/api/AuthApiService.js
+++ soll-parking/src/api/AuthApiService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Rest API 요청
 const apiClient = axios.create(
     {
-        baseURL : 'http://localhost:5011/auth'
+        baseURL : 'http://ec2-52-79-59-207.ap-northeast-2.compute.amazonaws.com:5011/auth'
     }
 )
 
--- soll-parking/src/api/ParkingLotApiService.js
+++ soll-parking/src/api/ParkingLotApiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Rest API 요청
 const apiClient = axios.create({
-    baseURL: "http://localhost:5011/parking-lot",
+    baseURL: "http://ec2-52-79-59-207.ap-northeast-2.compute.amazonaws.com:5011/parking-lot",
 });
 
 export const getAroundParkingLot = (location) => {
